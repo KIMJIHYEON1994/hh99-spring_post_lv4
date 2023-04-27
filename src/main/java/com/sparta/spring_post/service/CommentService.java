fa2d02273077f478aeb2110bc8383253c56ef8bf
LEOJI <kjh11-07@hanmail.net>
2023-04-27 23:31:02 +0900

    Refactor : 불필요한 주석 및 토큰 체크 제거

--- src/main/java/com/sparta/spring_post/service/CommentService.java
+++ src/main/java/com/sparta/spring_post/service/CommentService.java
@@ -5,25 +5,18 @@ import com.sparta.spring_post.dto.UserResponseDto;
 import com.sparta.spring_post.entity.Comment;
 import com.sparta.spring_post.entity.Post;
 import com.sparta.spring_post.entity.Users;
-import com.sparta.spring_post.jwt.JwtUtil;
 import com.sparta.spring_post.repository.CommentRepository;
 import com.sparta.spring_post.repository.PostRepository;
-import com.sparta.spring_post.repository.UserRepository;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
     private final PostRepository postRepository;
-    private final JwtUtil jwtUtil;
 
     // 댓글 등록
     @Transactional
@@ -69,29 +62,4 @@ public class CommentService {
 
     }
 
-
-    // 토큰 체크
-    public Users checkJwtToken(HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
-
-        // 토큰이 있는 경우에만 게시글 접근 가능
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
-
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            Users user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-            return user;
-
-        }
-        return null;
-    }
 }
